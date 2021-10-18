3ea39c496958283e7bb9f95d8d1cdf5262f049d8
ShawonMridha <you@example.com>
2021-10-18 22:28:19 +0600

    real project

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,73 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
 
 
 const Login = () => {
-    const {signInUsingGoogle} = useAuth();
+    const {signInUsingGoogle, handleRegistration,handleEmailChange, handlePasswordChange, error, toggleLogin,isLogin, handleResetPassword} = useAuth();
     return (
         <div>
+
+
+
+<Container>
+         <Form onSubmit={handleRegistration}>
+             <h3 className="text-primary">Please {isLogin ? 'Login' : 'Register'}</h3>
+  <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+    <Form.Label column sm={2}>
+      Email
+    </Form.Label>
+    <Col sm={10}>
+      <Form.Control onBlur={handleEmailChange} type="email" placeholder="Email" required />
+    </Col>
+  </Form.Group>
+
+  <Form.Group as={Row} className="mb-3" controlId="formHorizontalPassword">
+    <Form.Label column sm={2}>
+      Password
+    </Form.Label>
+    <Col sm={10}>
+      <Form.Control onBlur={handlePasswordChange} type="password" placeholder="Password" required />
+    </Col>
+  </Form.Group>
+  <fieldset>
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label as="legend" column sm={2}>
+      </Form.Label>
+     
+    </Form.Group>
+  </fieldset>
+  
+  <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+    <Col sm={{ span: 10, offset: 2 }}>
+      <Form.Check onChange={toggleLogin} label="Already Registered" />
+    </Col>
+  </Form.Group>
+
+
+  <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+    <Col sm={{ span: 10, offset: 2 }}>
+     
+    </Col>
+  </Form.Group>
+   <div className="row mb-3 text-danger">{error}</div>
+  <Form.Group as={Row} className="mb-3">
+    <Col sm={{ span: 10, offset: 2 }}>
+      <Button className="primary" type="submit">{isLogin ? 'Login' : 'Register'}</Button>
+    </Col>
+    <button type="button" onClick={handleResetPassword} className="btn btn-primary btn-sm">Reset password</button>
+  </Form.Group>
+</Form>
+  </Container>
+
+            
+
+
+            <div>---------------------------------------</div>
+            <br /> <br /> <br />
             <h3>Please Login</h3>
             <button onClick={signInUsingGoogle}>Google Sign In</button>
             <br />
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import initializeAuthentication from "../Firebase/firebase.init";
-import { getAuth, signInWithPopup, GoogleAuthProvider,  onAuthStateChanged, signOut} from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider,  onAuthStateChanged, signOut, createUserWithEmailAndPassword ,signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail} from "firebase/auth";
 
 initializeAuthentication();
 const useFairebase=()=>{
     const [user, setUser] = useState({});
     const[error, setError] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword]= useState('');
+    const[isLogin, setIsLogin]=useState(false);
 
 
     const auth = getAuth();
@@ -37,11 +40,91 @@ const useFairebase=()=>{
             }
         })
     },[])
+
+
+    const handleRegistration = e =>{
+        e.preventDefault();
+        console.log(email, password);
+        if(password.length < 6){
+            setError('password Must be at least 6 characters long')
+            return;
+        }
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+            setError('password Must contain 2 upper case');
+            return;
+        }
+        // createUserWithEmailAndPassword(auth, email, password)
+        // .then(result=>{
+        //     const user = result.user;
+        //     console.log(user);
+        //     setError('');
+        // })
+        // .catch(error=>{
+        //     setError(error.message);
+        // })
+
+        isLogin? processLogin(email,password): createNewUser(email, password);
+        
+    }
+    const processLogin = (email, password)=>{
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            setError('');
+        })
+        .catch(error=>{
+            setError(error.message)
+        })
+    }
+
+    const createNewUser=(email, password)=>{
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user);
+            setError('');
+            verifyEmail();
+        })
+        .catch(error=>{
+            setError(error.message);
+        })
+    }
+
+    const handleEmailChange = e=>{
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = e=>{
+        setPassword(e.target.value)
+    }
+
+    const toggleLogin = e=>{
+        setIsLogin(e.target.checked)
+    }
+
+    const verifyEmail=()=>{
+        sendEmailVerification(auth.currentUser)
+        .then(result=>{
+            console.log(result);
+        })
+    }
+
+    const handleResetPassword = ()=>{
+        sendPasswordResetEmail(auth, email)
+        .then(result =>{ })
+    }
     return{
         user,
         error,
         signInUsingGoogle,
-        logout
+        logout,
+        handleRegistration,
+        handleEmailChange,
+        handlePasswordChange,
+        toggleLogin,
+        isLogin,
+        handleResetPassword
     }
 }
 
